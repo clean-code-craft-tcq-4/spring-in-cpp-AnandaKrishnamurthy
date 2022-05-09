1e99e2023b7536ad8a98b63eb57b514357703f5d
AnandaKrishnamurthy <105204209+AnandaKrishnamurthy@users.noreply.github.com>
2022-05-09 20:33:43 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,7 +2,7 @@
 
  double Statistics::ComputeStatistics(const std::vector<double>& ) {
     //Implement statistics here
-  double average=(1.5+8.9+3.2+4.5)/4;
-  double max=8.9;
-  double min=1.5;
+  average=(1.5+8.9+3.2+4.5)/4;
+   max=8.9;
+   min=1.5;
 }
