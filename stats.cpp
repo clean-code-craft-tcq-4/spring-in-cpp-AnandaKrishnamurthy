803559fafa77466179bd6433836c38261cc1f212
AnandaKrishnamurthy <105204209+AnandaKrishnamurthy@users.noreply.github.com>
2022-05-09 19:48:09 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,5 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+ Statistics::ComputeStatistics(const std::vector<double>& ) {
     //Implement statistics here
 }
