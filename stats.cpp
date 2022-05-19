4f8adba346b01e47e00821c37e408c272a9d35f9
AnandaKrishnamurthy <105204209+AnandaKrishnamurthy@users.noreply.github.com>
2022-05-19 23:29:12 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,8 +1,49 @@
-#include "stats.h"
+#include "Stats.h"
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <math.h> 
+#include <vector>
 
- double Statistics::ComputeStatistics(const std::vector<double>& ) {
-    //Implement statistics here
-  average=(1.5+8.9+3.2+4.5)/4;
-   max=8.9;
-   min=1.5;
+using namespace Statistics;
+using namespace std;
+ComputeStatistics::ComputeStatistics(const std::vector<float>&numbers) {
+       
+    if (numbers.empty())        
+        {            
+        max = NAN;
+        min = NAN;
+        average=NAN; 
+        }
+    else
+        {
+        max = *max_element(numbers.begin(), numbers.end());
+        min = *min_element(numbers.begin(), numbers.end());
+        average=std::accumulate(numbers.begin(), numbers.end(), 0.0) / numbers.size();        
+        }  
 }
+StatsAlerter::StatsAlerter(double maxThresholds, const std:: vector<IAlerter*> &alerters){
+    MaxThresholds=maxThresholds;    
+    Alerters= alerters; 
+    }
+
+ StatsAlerter :: checkAndAlert(const std:: vector<float> &Temperaturedata)
+        {
+            double maximum = *max_element(Temperaturedata.begin(), Temperaturedata.end());
+            if (maximum >= MaxThresholds)
+            {
+                EmailAlert TempemailAlert;
+                LEDAlert TempledAlert;
+                TempemailAlert.Alert();
+                TempledAlert.Alert();
+               
+            }
+        }
+
+        
+
+
+       
+
+      
+
